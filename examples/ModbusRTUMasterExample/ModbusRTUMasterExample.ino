9930f6041b4abe39a6f3dd3d126d6a86aa04c5e4
chris <chrisbulliner@gmail.com>
2024-08-31 23:38:41 -0400

    Updated ModbusRTUMasterExample, uncluding adding an epic ASCII schematic.

--- examples/ModbusRTUMasterExample/ModbusRTUMasterExample.ino
+++ examples/ModbusRTUMasterExample/ModbusRTUMasterExample.ino
@@ -1,62 +1,137 @@
 /*
-  ModbusRTUMasterExample
-  
-  This example demonstrates how to setup and use the ModbusRTUMaster library (https://github.com/CMB27/ModbusRTUMaster).
-  It is intended to be used with a second board running ModbusRTUSlaveExample from the ModbusRTUSlave library (https://github.com/CMB27/ModbusRTUSlave).  
-  
-  Circuit:
-  - The center pin of a potentiometer to pin A0, and the outside pins of the potentiometer to your board's logic level voltage (5V or 3.3V) and GND
-  - The center pin of a potentiometer to pin A1, and the outside pins of the potentiometer to your board's logic level voltage (5V or 3.3V) and GND
-  - A pushbutton switch from pin 2 to GND
-  - A pushbutton switch from pin 3 to GND
-  - A LED from pin 5 to GND with a 1K ohm series resistor
-  - A LED from pin 6 to GND with a 1K ohm series resistor
-  - A LED from pin 7 to GND with a 1K ohm series resistor
-  - A LED from pin 8 to GND with a 1K ohm series resistor
-  - RX pin (typically pin 0, pin 10 if using SoftwareSerial) to TX pin of the master/client board
-  - TX pin (typically pin 1, pin 11 if using SoftwareSerial) to RX pin of the master/client board
-  - GND to GND of the master/client board
-  - Pin 13 is set up as the driver enable pin. This pin will be HIGH whenever the board is transmitting.
-
-  !NOTE
-  Both boards will also need power.
-  
-  Created: 2023-07-22
-  By: C. M. Bulliner
-  Last Modified: 2024-08-30
-  By: C. M. Bulliner
-  
-*/
+ * ModbusRTUMasterExample
+ *
+ * This example demonstrates how to setup and use the ModbusRTUMaster library (https://github.com/CMB27/ModbusRTUMaster).
+ * This is a communications library, so in order to test it, you will need something to communicate with.
+ * A second board running ModbusRTUSlaveExample from the ModbusRTUSlave library (https://github.com/CMB27/ModbusRTUSlave) will be needed.
+ * This second board will also need to be setup with the circuit shown below.
+ * 
+ *
+ * Circuit:
+ *                                                  VCC                                                                 0.1µF Capacitor
+ *                                                   ^                            Arduino Board                        /
+ *                                                   |                           /                                  | |
+ *  +--------------------o---------------------------o--------------------------/-------------------------------o---| |---+
+ *  |                    |                                                     /                                |   | |   |
+ *  |                    |         +------+                   +------+        /            RS-485               |         |
+ *  |                    |      +--|      |-------------------|      |-------+        Transceiver               |         |
+ *  |                    |      |  |      |                   +------+       |                    \             |         |               120 Ω Resistor
+ *  |                    |      |  |      |                                  |              +-------v-------+   |         |        _____ /
+ *  |                    |      |  +------+                          SCL [ ] |      +-------| RO        VCC |---+         |   +---|_____|---+
+ *  |                    |      |                                    SDA [ ] |      |       |               |             |   |             |
+ *  |                    |      |                                   AREF [ ] |      |   +---| RE          B |-------------|---o-------------|---<> RS485_D-
+ *  |                    |      |                                    GND [ ] |      |   |   |               |             |                 |
+ *  |                    |      | [ ] BOOT                            13 [ ] |------|---o---| DE          A |-------------|-----------------o---<> RS485_D+
+ *  |                    +------| [ ] IOREF                           12 [ ] |      |       |               |             |
+ *  |                           | [ ] RESET                          ~11 [ ] |      |   +---| DI        GND |-------------o
+ *  |                           | [ ] 3.3V                           ~10 [ ] |      |   |   +---------------+             |
+ *  |                           | [ ] 5V                              ~9 [ ] |      |   |                       _____     |
+ *  |                           | [ ] GND                              8 [ ] |------|---|----------------->|---|_____|----o
+ *  |                    +------| [ ] GND                                    |      |   |                       _____     |
+ *  |                    |      | [ ] VIN                              7 [ ] |------|---|----------------->|---|_____|----o
+ *  |                    |      |                                     ~6 [ ] |------|---|------------+                    |  4x LEDs
+ *  |      +--------------------| [ ] A0                              ~5 [ ] |------|---|---------+  |          _____     |  4x 1K Ω Resistors
+ *  |      |      +-------------| [ ] A1                               4 [ ] |      |   |         |  +---->|---|_____|----o
+ *  |    __v__    |      |      | [ ] A2                              ~3 [ ] |------|---|------+  |             _____     |
+ *  o---|_____|---|------o      | [ ] A3                               2 [ ] |------|---|---+  |  +------->|---|_____|----o
+ *  |             |      |      | [ ] A4                            TX→1 [ ] |------|---+   |  |                          |
+ *  |           __v__    |      | [ ] A5                            RX←0 [ ] |------+       |  |                          |
+ *  +----------|_____|---o      |                                 __________/               |  |             __|__        |
+ *                       |       \_______________________________/                          |  +-------------o   o--------o
+ *        2x 10K Ω       |                                                                  |                             |  2x Pushbutton Switches
+ *     Potentiometers    |                                                                  |                __|__        |
+ *                       |                                                                  +----------------o   o--------o
+ *                       |                                                                                                |
+ *                       +------------------------------------------------------------------------------------------------o---------------------<> GND
+ *
+ * Components:
+ * - Arduino Board
+ * - Half-Duplex RS-485 Transceiver (Must be able to operate at your arduino board's logic level)
+ * - 120 Ω Resistor (At least 1/4W recommended)
+ * - 4x 1K Ω Resistors
+ * - 2x 10K Ω Potentiometers
+ * - 0.1µF Capacitor
+ * - 4x LEDs
+ * 
+ * If using an ATmega328P or ATmega168 based board (Arduino UNO R3 and earlier, Arduino Nano, Arduino Pro, etc.):
+ * - Connect RO to pin 10 instead of pin 0
+ * - Connect DI to pin 11 instead of pin 1
+ * 
+ * If using an Arduino Mega (either variant) or Arduino Due:
+ * - Connect RO to pin 19 instead of pin 0
+ * - Connect DI to pin 18 instead of pin 1
+ * 
+ * Connect the following together from this circuit and the one for ModbusRTUSlaveExample:
+ * - RS485_D-
+ * - RS485_D+
+ * - GND
+ * 
+ * 
+ * Created: 2023-07-22
+ * By: C. M. Bulliner
+ * Last Modified: 2024-08-31
+ * By: C. M. Bulliner
+ * 
+ */
 
 #include <ModbusRTUMaster.h>
 
-#if defined(__AVR_ATmega328P__) || defined(__AVR_ATmega168__)
-  #include <SoftwareSerial.h>
-  #define USE_SOFTWARE_SERIAL
-#endif
+// You can change the baud and config values if you like.
+// Just make sure they match the settings you use in ModbusRTUSlaveExample.
+#define MODBUS_BAUD 38400
+#define MODBUS_CONFIG SERIAL_8N1
 
-#if (defined(ARDUINO_NANO_RP2040_CONNECT) && !defined(ARDUINO_ARCH_MBED)) || defined(ARDUINO_NANO_ESP32)
-  const int8_t buttonPins[2] = {D2, D3};
-  const int8_t ledPins[4] = {D5, D6, D7, D8};
-  const int8_t dePin = D13;
+// Pre delay is a microseconds delay after the DE and RE pins go HIGH and before Serial transmission begins.
+// Post delay is a microseconds delay after Serial transmission ends and before the DE and RE pins go LOW.
+// Normally you would leave these at 0, but there are several boards that need a post delay in order for the library to function.
+#define MODBUS_PRE_DELAY 0
+#if defined(ARDUINO_UNOR4_MINIMA) || defined(ARDUINO_UNOR4_WIFI) || defined(ARDUINO_GIGA) || (defined(ARDUINO_NANO_RP2040_CONNECT) && defined(ARDUINO_ARCH_MBED))
+  // Serial1.flush() for these boards or board/core combination returns one byte too soon.
+  // This messes up the operation of the DE and RE pins, causing communication errors.
+  // A postDelay of one byte length is calculated here.
+  #if (MODBUS_CONFIG == SERIAL_8E2) || (MODBUS_CONFIG == SERIAL_8O2)
+    #define MODBUS_BITS_PER_CHAR 12
+  #elif (MODBUS_CONFIG == SERIAL_8N2) || (MODBUS_CONFIG == SERIAL_8E1) || (MODBUS_CONFIG == SERIAL_8O1)
+    #define MODBUS_BITS_PER_CHAR 11
+  #else
+    #define MODBUS_BITS_PER_CHAR 10
+  #endif
+  #define MODBUS_POST_DELAY ((MODBUS_BITS_PER_CHAR * 1000000) / MODBUS_BAUD)
 #else
-  const int8_t buttonPins[2] = {2, 3};
-  const int8_t ledPins[4] = {5, 6, 7, 8};
-  const int8_t dePin = 13;
+  #define MODBUS_POST_DELAY 0
 #endif
-const int8_t knobPins[2] = {A0, A1};
-const int8_t rePin = -1;
 
-#if defined(USE_SOFTWARE_SERIAL)
+#if defined(__AVR_ATmega328P__) || defined(__AVR_ATmega168__)
+  // The ATmega328P and ATmega168 only have one HardwareSerial port, and on Arduino boards it is usually connected to a USB/UART bridge.
+  // So, for these boards, we will use SoftwareSerial with the lbrary, leaving the HardwareSerial port available to send debugging messages.
+  #include <SoftwareSerial.h>
   const int8_t rxPin = 10;
   const int8_t txPin = 11;
   SoftwareSerial mySerial(rxPin, txPin);
   #define MODBUS_SERIAL mySerial
 #elif defined(ARDUINO_NANO_ESP32)
+  // On the Arduino Nano ESP32, the HardwareSerial port on pins 0 and 1 is Serial0
   #define MODBUS_SERIAL Serial0
 #else
+  // On the majority of Arduino boards, the HardwareSerial port on pins 0 and 1 is Serial1
+  // On the Arduino Mega and Adruino Due, Serial1 is on pins 18 and 19.
   #define MODBUS_SERIAL Serial1
 #endif
+
+#if (defined(ARDUINO_NANO_RP2040_CONNECT) && !defined(ARDUINO_ARCH_MBED)) || defined(ARDUINO_NANO_ESP32)
+  // These boards operate unsing GPIO numbers that don't correspond to the numbers on the boards.
+  // However they do have D# values #defined to correct this.
+  const int8_t buttonPins[2] = {D2, D3};
+  const int8_t ledPins[4] = {D5, D6, D7, D8};
+  const int8_t dePin = D13;
+#else
+  // Other boards do not have D# values, and will throw an error if you try to use them.
+  const int8_t buttonPins[2] = {2, 3};
+  const int8_t ledPins[4] = {5, 6, 7, 8};
+  const int8_t dePin = 13;
+#endif
+const int8_t knobPins[2] = {A0, A1};
+const int8_t rePin = -1; // -1 means "Don't use this"
 
 ModbusRTUMaster modbus(MODBUS_SERIAL, dePin, rePin);
 
@@ -73,7 +148,7 @@ uint16_t inputRegisters[numInputRegisters];
 unsigned long transactionCounter = 0;
 unsigned long errorCounter = 0;
 
-const char* errorString[] = {
+const char* errorStrings[] = {
   "success",
   "invalid id",
   "invalid buffer",
@@ -96,16 +171,10 @@ const char* errorString[] = {
 
 void printLog(uint8_t unitId, uint8_t functionCode, uint16_t startingAddress, uint16_t quantity, uint8_t error) {
   transactionCounter++;
-  if (error) {
-    errorCounter++;
-  }
+  if (error) errorCounter++;
   char string[128];
-  if (error == MODBUS_RTU_MASTER_EXCEPTION_RESPONSE) {
-    sprintf(string, "%ld %ld %02X %02X %04X %04X %s: %02X\n", transactionCounter, errorCounter, unitId, functionCode, startingAddress, quantity, errorString[error], modbus.getExceptionResponse());
-  }
-  else {
-    sprintf(string, "%ld %ld %02X %02X %04X %04X %s\n", transactionCounter, errorCounter, unitId, functionCode, startingAddress, quantity, errorString[error]);
-  }
+  if (error == MODBUS_RTU_MASTER_EXCEPTION_RESPONSE) sprintf(string, "%ld %ld %02X %02X %04X %04X %s: %02X\n", transactionCounter, errorCounter, unitId, functionCode, startingAddress, quantity, errorStrings[error], modbus.getExceptionResponse());
+  else sprintf(string, "%ld %ld %02X %02X %04X %04X %s\n", transactionCounter, errorCounter, unitId, functionCode, startingAddress, quantity, errorStrings[error]);
   Serial.print(string);
 }
 
@@ -120,52 +189,29 @@ void setup() {
   pinMode(ledPins[1], OUTPUT);
   pinMode(ledPins[2], OUTPUT);
   pinMode(ledPins[3], OUTPUT);
+
+  #if defined(ARDUINO_NANO_ESP32)
+    analogReadResolution(10);
+  #endif
   
   Serial.begin(115200);
 
-  unsigned long baud = 38400;
-  uint32_t config = SERIAL_8N1;
-  unsigned long preDelay = 0;
-  unsigned long postDelay = 0;
-
-  // WORKAROUND 1
-  // Serial1.flush() for the following boards or board/core combination returns one byte too soon.
-  // A postDelay of one byte length is calculated here. This delay is is used to keep the dePin HIGH until the full message is sent.
-  #if defined(ARDUINO_UNOR4_MINIMA) || defined(ARDUINO_UNOR4_WIFI) || defined(ARDUINO_GIGA) || (defined(ARDUINO_NANO_RP2040_CONNECT) && defined(ARDUINO_ARCH_MBED))
-    unsigned long bitsPerChar;
-    switch (config) {
-      case SERIAL_8E2:
-      case SERIAL_8O2:
-        bitsPerChar = 12;
-        break;
-      case SERIAL_8N2:
-      case SERIAL_8E1:
-      case SERIAL_8O1:
-        bitsPerChar = 11;
-        break;
-      case SERIAL_8N1:
-      default:
-        bitsPerChar = 10;
-        break;
-    }
-    postDelay = ((bitsPerChar * 1000000) / baud);
-  #endif
-  
-  #if defined(USE_SOFTWARE_SERIAL)
-    MODBUS_SERIAL.begin(baud);
-    modbus.begin(baud, SERIAL_8N1, preDelay, postDelay);
+  // The Serial port used with ModbusRTUMaster is started separately from the library.
+  // This is for broader board and processor compatibility
+  #if defined(__AVR_ATmega328P__) || defined(__AVR_ATmega168__)
+    // SoftwareSerial on an AVR only supports SERIAL_8N1.
+    MODBUS_SERIAL.begin(MODBUS_BAUD);
   #else
-    MODBUS_SERIAL.begin(baud, config);
+    MODBUS_SERIAL.begin(MODBUS_BAUD, MODBUS_CONFIG);
+  #endif
 
-    // WORKAROUND 2
-    // The following board/core combination does not get the timings right for receiving the modbus data.
+  #if (defined(ARDUINO_NANO_RP2040_CONNECT) && defined(ARDUINO_ARCH_MBED))
+    // This board/core combination does not get the timings right for receiving modbus data.
     // Telling the ModbusRTUMaster library that we are operating at a lower baud rate, causes it to loosen the timing tolerances for receiving messages.
-    #if (defined(ARDUINO_NANO_RP2040_CONNECT) && defined(ARDUINO_ARCH_MBED))
-      modbus.begin((baud / 4), config, preDelay, postDelay);
-    #else
-      modbus.begin(baud, config, preDelay, postDelay);
-    #endif
-    
+    // This is less than ideal, but it works.
+    modbus.begin(MODBUS_BAUD / 4, MODBUS_CONFIG, MODBUS_PRE_DELAY, MODBUS_POST_DELAY);
+  #else
+    modbus.begin(MODBUS_BAUD, MODBUS_CONFIG, MODBUS_PRE_DELAY, MODBUS_POST_DELAY);
   #endif
 }
 
