6e39195cd77cd885e184927f1a8fa0aed6d32083
Chris Bulliner <chrisbulliner@gmail.com>
2024-09-03 22:03:46 -0400

    Added table to example program noting boards tested.

--- examples/ModbusRTUMasterExample/ModbusRTUMasterExample.ino
+++ examples/ModbusRTUMasterExample/ModbusRTUMasterExample.ino
@@ -6,6 +6,43 @@
  * A second board running ModbusRTUSlaveExample from the ModbusRTUSlave library (https://github.com/CMB27/ModbusRTUSlave) will be needed.
  * This second board will also need to be setup with the circuit shown below.
  * 
+ * This program has been tested with the following boards:
+ * +-----------------------------+----------------------------------------------+-------------------------+--------+-------+--------+--------+----------------+------------+
+ * | Board Name                  | Core                                         | By                      | Ver.   | Works | RX Pin | TX Pin | Port           | Post Delay |
+ * +-----------------------------+----------------------------------------------+-------------------------+--------+-------+--------+--------+----------------+------------+
+ * | Arduino Due                 | Arduino SAM Boards (32-bits ARM Cortex-M3)   | Arduino                 | 1.6.12 | Yes   |     19 |     18 | Serial1        |            |
+ * | Arduino Giga                | Arduino Mbed OS GIGA Boards                  | Arduino                 |  4.1.5 | Yes   |      0 |      1 | Serial1        | Yes        |
+ * | Arduino Leonardo            | Arduino AVR Boards                           | Arduino                 |  1.8.6 | Yes   |      0 |      1 | Serial1        |            |
+ * | Arduino Make Your UNO       | Arduino AVR Boards                           | Arduino                 |  1.8.6 | Yes   |     10 |     11 | SoftwareSerial |            |
+ * | Arduino Mega 2560           | Arduino AVR Boards                           | Arduino                 |  1.8.6 | Yes   |     19 |     18 | Serial1        |            |
+ * | Arduino Nano                | Arduino AVR Boards                           | Arduino                 |  1.8.6 | Yes   |     10 |     11 | SoftwareSerial |            |
+ * | Arduino Nano 33 BLE         | Arduino Mbed OS Nano Boards                  | Arduino                 |  4.1.5 | Yes   |      0 |      1 | Serial1        |            |
+ * | Arduino Nano 33 IoT         | Arduino SAMD Boards (32-bits ARM Cortex-M0+) | Arduino                 | 1.8.14 | Yes   |      0 |      1 | Serial1        |            |
+ * | Arduino Nano ESP32          | Arduino ESP32 Boards                         | Arduino                 | 2.0.13 | Yes   |      0 |      1 | Serial0        |            |
+ * | Arduino Nano ESP32          | esp32                                        | Espressif Systems       |  3.0.4 | Yes   |      0 |      1 | Serial0        |            |
+ * | Arduino Nano Every          | Arduino megaAVR Boards                       | Arduino                 |  1.8.8 | Yes   |      0 |      1 | Serial1        |            |
+ * | Arduino Nano Matter         | Silicon Labs                                 | Silicon Labs            |  2.1.0 | No    |      0 |      1 | Serial1        |            |
+ * | Arduino Nano RP2040 Connect | Arduino Mbed OS Nano Boards                  | Arduino                 |  4.1.5 | Yes   |      0 |      1 | Serial1        | Yes        |
+ * | Arduino Nano RP2040 Connect | Raspberry Pi Pico/RP2040                     | Earle F. Philhower, III |  4.0.1 | Yes   |      0 |      1 | Serial1        |            |
+ * | Arduino UNO R3 SMD          | Arduino AVR Boards                           | Arduino                 |  1.8.6 | Yes   |     10 |     11 | SoftwareSerial |            |
+ * | Arduino UNO R4 Minima       | Arduino UNO R4 Boards                        | Arduino                 |  1.2.0 | Yes   |      0 |      1 | Serial1        | Yes        |
+ * +-----------------------------+----------------------------------------------+-------------------------+--------+-------+--------+--------+----------------+------------+
+ * 
+ * Note on Arduino Leonardo:
+ * Closing a Serial Monitor connected to the board while running this program will cause Modbus communication errors.
+ * Resetting the board should cause normal Modbus communications to resume.
+ * 
+ * Note on Arduino Nano ESP32:
+ * When using the "esp32" core by Espressif Systems, please set pin numbering to "By GPIO number (legacy)".
+ * The pin numbering option, "By Arduino pin (default)" does not appear to work correctly with this core.
+ *
+ * Note on Arduino Nano RP2040 Connect:
+ * When using the "Arduino Mbed OS Nano Boards" core by Arduino, this board has some trouble receiving Modbus messages.
+ * A workaround is implemented in this program, but it reduces the strictness of the checks on the received messages.
+ *
+ * Note on Arduino Nano Matter:
+ * From what I can tell flush() is not properly implemented with Serial on this board.
+ * ModbusRTUMaster depends on flush() to know when to set the DE and RE pins LOW.
  *
  * Circuit:
  *                                                  VCC                                                                 0.1µF Capacitor
@@ -53,7 +90,7 @@
  * - 0.1µF Capacitor
  * - 4x LEDs
  * 
- * If using an ATmega328P or ATmega168 based board (Arduino UNO R3 and earlier, Arduino Nano, Arduino Pro, etc.):
+ * If using an ATmega328P or ATmega168 based board (Arduino UNO R3 and earlier, Arduino Nano, Arduino Pro Mini, etc.):
  * - Connect RO to pin 10 instead of pin 0
  * - Connect DI to pin 11 instead of pin 1
  * 
@@ -69,7 +106,7 @@
  * 
  * Created: 2023-07-22
  * By: C. M. Bulliner
- * Last Modified: 2024-08-31
+ * Last Modified: 2024-09-03
  * By: C. M. Bulliner
  * 
  */
